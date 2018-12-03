27b8847cfe41c47fa16b546de793df6e4122d4a0
Kevin <kevinkloet@outlook.com>
2018-12-03 19:54:49 +0100

    GET and DELETE tests

--- game.controller.test.js
+++ game.controller.test.js
@@ -9,7 +9,7 @@ const endpointToTest = '/api/games'
 
 describe('Games API POST', () => {
     it('should return a valid game when posting a valid object', (done) => {
- 
+
         chai.request(server)
             .post(endpointToTest)
             .send({
@@ -31,7 +31,7 @@ describe('Games API POST', () => {
                 Games.should.have.property('email').equals('user@host.com')
                 Games.should.not.have.property('password')
                 done()
-        })
+            })
     })
 
     it('should throw an error when using invalid JWT token', (done) => {
@@ -96,9 +96,45 @@ describe('Games API POST', () => {
 })
 
 describe('Games API GET', () => {
-    it.skip('should return an array of Gamess', (done) => {
-        // Write your test here
-        done()
+    it('should return an array of Games', (done) => {
+        chai.request(server)
+            .get(endpointToTest)
+            .end(function (err, res) {
+                // Check the response
+                res.body.should.be.a('array')
+                res.body[0].should.have.property('id').which.is.a('number')
+                res.body[0].should.have.property('name').which.is.a('string')
+                res.body[0].should.have.property('producer').which.is.a('string')
+                res.body[0].should.have.property('year').which.is.a('number')
+                res.body[0].should.have.property('type').which.is.a('string')
+                expect(err).to.be.null
+                expect(res).to.have.status(200)
+                if (err != null) {
+                    assert(false, err)
+                }
+                assert(true)
+                done()
+            })
+    })
+    it('should return the game with the :id', (done) => {
+        chai.request(server)
+            .get(endpointToTest + '/0')
+            .end(function (err, res) {
+                //Check the request
+                res.body.should.be.a('object')
+                res.body.should.have.property('id').which.is.a('number').which.eql(0)
+                res.body.should.have.property('name').which.is.a('string')
+                res.body.should.have.property('producer').which.is.a('string')
+                res.body.should.have.property('year').which.is.a('number')
+                res.body.should.have.property('type').which.is.a('string')
+                expect(err).to.be.null
+                expect(res).to.have.status(200)
+                if (err != null) {
+                    assert(false, err)
+                }
+                assert(true)
+                done();
+            })
     })
 
 })
@@ -147,9 +183,45 @@ describe('Games API PUT', () => {
 })
 
 describe('Games API DELETE', () => {
+    var gametest = new game(100, 'DeleteTest', 'DeleteCompany', 2018, 'RPG')
     it.skip('should return http 200 when deleting a Games with existing id', (done) => {
-        // Write your test here
-        done()
-    })
+        chai.request(server)
+            .post(endpointToTest)
+            .send(gametest)
+            .end((err, res) => {
+                // Check properties of object in the request
+                gametest.name.should.be.a('string')
+                gametest.producer.should.be.a('string')
+                gametest.year.should.be.a('number')
+                gametest.type.should.be.a('string')
 
+                // Check the response
+                res.body.should.be.a('object')
+                res.body.should.have.property('name').eql(gametest.name).which.is.a('string').to.have.lengthOf(gametest.name.length)
+                res.body.should.have.property('producer').eql(gametest.producer).which.is.a('string').to.have.lengthOf(gametest.producer.length)
+                res.body.should.have.property('year').eql(gametest.year).which.is.a('number')
+                res.body.should.have.property('type').eql(gametest.type).which.is.a('string').to.have.lengthOf(gametest.type.length)
+
+                // Check if post was a succes
+                expect(res).to.have.status(200)
+                expect(err).to.be.null
+
+                // Delete the one we just added
+                chai.request(server).del(endpointToTest + "/100").end(err, res, next => {
+                    expect(res).to.have.status(200)
+                    response.should.be.a('object')
+                    res.body.should.have.property('name').eql(gametest.name).which.is.a('string').to.have.lengthOf(gametest.name.length)
+                    res.body.should.have.property('producer').eql(gametest.producer).which.is.a('string').to.have.lengthOf(gametest.producer.length)
+                    res.body.should.have.property('year').eql(gametest.year).which.is.a('number')
+                    res.body.should.have.property('type').eql(gametest.type).which.is.a('string').to.have.lengthOf(gametest.type.length)
+                    next
+                })
+                if (err != null) {
+                    assert(false, err)
+                } else {
+                    assert(true, 'De game is verwijderd')
+                }
+                done()
+            })
+    })
 })
