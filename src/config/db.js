08f3c7637e0dbb5efb959ac7f6ee7fdd109bc344
DESKTOP-KEVIN\kevin <212222@student.scalda.nl>
2018-12-04 10:19:21 +0100

    sql server integration

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+const dbconfig = {
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'gamedb_user',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE || 'gamedb',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+};
+
+const pool = mysql.createPool(dbconfig);
+
+console.log(`Connected to database ${dbconfig.database} on host ${dbconfig.host}`);
+
+module.exports = pool;
